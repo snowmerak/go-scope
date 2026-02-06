50af77afdd1093a748fe03bfad9baf0d9b976662
snowmerak <snowmerak@outlook.com>
2026-02-06 20:27:11 +0900

    refactor(scope): enhance error handling in With function

--- scope.go
+++ scope.go
@@ -24,15 +24,31 @@ func Catch[I, O any](fn func(context.Context, I) (O, error)) func(context.Contex
 }
 
 func With[I, O any](fn func(context.Context, func(io.Closer), I) (O, error)) func(context.Context, I) (O, error) {
-	return func(ctx context.Context, input I) (O, error) {
+	return func(ctx context.Context, input I) (output O, err error) {
 		errs := make([]error, 0, 4)
+
+		defer func() {
+			if r := recover(); r != nil {
+				if e, ok := r.(error); ok {
+					e := fmt.Errorf("panic caught: %w", e)
+					errs = append(errs, e)
+				} else {
+					e := fmt.Errorf("panic caught: %+v", r)
+					errs = append(errs, e)
+				}
+
+				output = *new(O) // zero value
+				err = errors.Join(errs...)
+			}
+		}()
+
 		capture := func(closer io.Closer) {
 			if err := closer.Close(); err != nil {
 				errs = append(errs, err)
 			}
 		}
 
-		output, err := fn(ctx, capture, input)
+		output, err = fn(ctx, capture, input)
 		if err != nil {
 			errs = append(errs, err)
 		}
